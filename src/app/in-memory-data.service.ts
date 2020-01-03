a5907bcc103462dc11e54de42d0bf5c007e8d537
Oualidben1 <oualid.bentaleb@PC01336.intitek.local>
2020-01-03 16:19:43 +0100

    first commit

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,28 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from "./Hero";
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root',
+  })
+  export class InMemoryDataService implements InMemoryDataService {
+    createDb() {
+      const heroes = [
+        { id: 11, name: 'Ichigo',rank: 'Shinigami', attak: 'Getsuga'},       
+        { id: 12, name: 'Vegeta',rank: 'Saiyajin',attak: 'Final Flash'},
+        { id: 13, name: 'Zouma',rank: 'Chien',attak: 'Tinker'},        
+        { id: 14, name: 'Saber',rank: 'Heroic',attak: 'Excalibur'},         
+        { id: 15, name: 'SuperGirl',rank: 'Kryptonien',attak: 'HeatVision'},
+        { id: 16, name: 'Sasuke',rank: 'Ninja',attak: 'ManngekyouSharingan'},
+        { id: 17, name: 'SuperMan',rank: 'Kryptonien',attak: 'Cold Breath'},
+        ];
+        return {heroes};
+    }
+     // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+    genId(heroes: Hero[]): number {
+      return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+    }
+  }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from "./Hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Ichigo', rank: 'Shinigami', attak: 'Getsuga'},       
+    { id: 12, name: 'Vegeta',rank: 'Saiyajin',attak: 'Final Flash'},
+    { id: 13, name: 'Zouma',rank: 'Chien',attak: 'Tinker'},        
+    { id: 14, name: 'Saber',rank: 'Heroic',attak: 'Excalibur'},         
+    { id: 15, name: 'SuperGirl',rank: 'Kryptonien',attak: 'HeatVision'},
+    { id: 16, name: 'Sasuke',rank: 'Ninja',attak: 'RasenShuriken'},
+    { id: 17, name: 'SuperMan',rank: 'Kryptonien',attak: 'Cold Breath'},
+ ];
+ 
