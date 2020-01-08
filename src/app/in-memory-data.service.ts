7e1de968a1c634ca002c9cac1faa551ef1d18c15
Oualidben1 <oualid.bentaleb@PC01336.intitek.local>
2020-01-08 16:03:06 +0100

    wip

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -7,12 +7,12 @@ import { Injectable } from '@angular/core';
   export class InMemoryDataService implements InMemoryDataService {
     createDb() {
       const heroes = [
-        { id: 11, name: 'Ichigo',rank: 'Shinigami', attak: 'Getsuga'},       
+        { id: 11, name: 'Zouma', rank: 'Chien', attak: 'Tinker'},       
         { id: 12, name: 'Vegeta',rank: 'Saiyajin',attak: 'Final Flash'},
-        { id: 13, name: 'Zouma',rank: 'Chien',attak: 'Tinker'},        
+        { id: 13, name: 'Ichigo',rank: 'Shinigami',attak: 'Getsuga'},        
         { id: 14, name: 'Saber',rank: 'Heroic',attak: 'Excalibur'},         
-        { id: 15, name: 'SuperGirl',rank: 'Kryptonien',attak: 'HeatVision'},
-        { id: 16, name: 'Sasuke',rank: 'Ninja',attak: 'ManngekyouSharingan'},
+        { id: 15, name: 'Sasuke',rank: 'Ninja',attak: 'MangekyouSharingan'},
+        { id: 16, name: 'SuperGirl',rank: 'Kryptonien',attak: 'HeatVision'},
         { id: 17, name: 'SuperMan',rank: 'Kryptonien',attak: 'Cold Breath'},
         ];
         return {heroes};
